06fe8bc264d6362ad96b4b7e5f5d859693604f36
Emmmanuel Lopez <jesusemmcik@gmail.com>
2024-07-05 00:00:55 -0600

    added localstorage

--- src/store/usePatient.ts
+++ src/store/usePatient.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { v4 as uuidv4 } from "uuid";
 import { Patient, PatientDraft } from "../types";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 type PatientState = {
   patients: Patient[];
@@ -16,26 +16,33 @@ const createPatient = (patient: PatientDraft): Patient => {
   return { ...patient, id: uuidv4() };
 };
 export const usePatientStore = create<PatientState>()(
-  devtools((set) => ({
-    patients: [],
-    activeId: "",
-    addPatient: (patient) => {
-      const newPatient = createPatient(patient);
-      set((state) => ({ patients: [...state.patients, newPatient] }));
-    },
-    deletePatient: (id) => {
-      set((state) => ({ patients: state.patients.filter((p) => p.id !== id) }));
-    },
-    getPatientById: (id) => {
-      set(() => ({ activeId: id }));
-    },
-    updatePatient: (patient) => {
-      set((state) => ({
-        patients: state.patients.map((p) =>
-          p.id === state.activeId ? { id: p.id, ...patient } : p
-        ),
+  devtools(
+    persist(
+      (set) => ({
+        patients: [],
         activeId: "",
-      }));
-    },
-  }))
+        addPatient: (patient) => {
+          const newPatient = createPatient(patient);
+          set((state) => ({ patients: [...state.patients, newPatient] }));
+        },
+        deletePatient: (id) => {
+          set((state) => ({
+            patients: state.patients.filter((p) => p.id !== id),
+          }));
+        },
+        getPatientById: (id) => {
+          set(() => ({ activeId: id }));
+        },
+        updatePatient: (patient) => {
+          set((state) => ({
+            patients: state.patients.map((p) =>
+              p.id === state.activeId ? { id: p.id, ...patient } : p
+            ),
+            activeId: "",
+          }));
+        },
+      }),
+      { name: "patientStorage" }
+    )
+  )
 );
